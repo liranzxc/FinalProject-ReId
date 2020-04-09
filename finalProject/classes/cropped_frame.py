07957a0c94557afb9fb4ec2a1da3cb3d7a5fcad8
nofar <nofaralfasi@gmail.com>
2020-04-09 20:24:13 +0300

    New changelist

--- finalProject/classes/cropped_frame.py
+++ finalProject/classes/cropped_frame.py
@@ -1,9 +1,10 @@
+import cv2
 # TODO consider adding the locations too
 
 
 class CroppedFrame:
     def __init__(self, frame_image):
         self.frame_image = frame_image
-        self.gray_image = None
+        self.gray_image = cv2.cvtColor(frame_image, cv2.COLOR_BGR2GRAY)
         self.frame_keypoints = {}
         self.frame_des = {}
